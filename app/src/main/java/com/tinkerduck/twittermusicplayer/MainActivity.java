e00585b34b211a0d5dde4ddbe1aeea7702f73bb3
Carl <m3chanical@tinkerduck.com>
2015-03-07 22:34:08 -0500

    add handler for continuous search of tweet mentions, mostly complete. currently only play by artist works.

--- app/src/main/java/com/tinkerduck/twittermusicplayer/MainActivity.java
+++ app/src/main/java/com/tinkerduck/twittermusicplayer/MainActivity.java
@@ -1,8 +1,10 @@
 package com.tinkerduck.twittermusicplayer;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,6 +14,7 @@ import android.widget.Toast;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.SessionManager;
 import com.twitter.sdk.android.core.TwitterApiClient;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterAuthToken;
@@ -23,8 +26,12 @@ import com.twitter.sdk.android.core.models.Tweet;
 import com.twitter.sdk.android.core.services.StatusesService;
 import com.twitter.sdk.android.tweetui.CompactTweetView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -35,35 +42,38 @@ import io.fabric.sdk.android.Fabric;
 public class MainActivity extends Activity {
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-    private static final String TWITTER_KEY = "0gufvnbrcuqDQcJ67ZOk3FpW1";
-    private static final String TWITTER_SECRET = "aiRAfpseomK5qqMZdpLrzc4FUB9DiQh1UG2E71UlzJs8IqEAfx";
+
+    private Handler handler = new Handler();
+    private int timeDelay = 1000 * 60 * 3; //3 minute delay, from miliseconds to seconds ;)
     public long lastMentionID = 1L;
+    public TwitterSession session;
     public String[] splitTweet;
-    public ArrayList<String> splitTweetList = new ArrayList<>();
+    public List<String> splitTweetList = new ArrayList<>();
     public MusicHandler musicHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         musicHandler = new MusicHandler(this);
+        startHandler(null);
 
         TwitterLoginButton loginButton = (TwitterLoginButton) findViewById(R.id.login_button);
         loginButton.setEnabled(true);
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
-
-        final LinearLayout myLayout = (LinearLayout) findViewById(R.id.tweet_layout);
+        if (Twitter.getSessionManager().getActiveSession() != null) {
+            session = Twitter.getSessionManager().getActiveSession();
+            getMentions();
+        }
         loginButton.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> twitterSessionResult) {
-                TwitterSession session =
-                        Twitter.getSessionManager().getActiveSession();
+                session = Twitter.getSessionManager().getActiveSession();
                 TwitterAuthToken authToken = session.getAuthToken();
                 final String token = authToken.token;
                 final String secret = authToken.secret;
-
-                getMentions(myLayout);
-
+                getMentions();
             }
             @Override
             public void failure(TwitterException e) {
@@ -72,41 +82,52 @@ public class MainActivity extends Activity {
         });
     }
 
-    private void getMentions(final LinearLayout myLayout) {
+    @Override
+    protected void onDestroy() {
+        handler.removeCallbacks(runnable);
+        super.onDestroy();
+    }
+
+    private void getMentions() {
+        final LinearLayout tweetLayout = (LinearLayout) findViewById(R.id.tweet_layout);
         TwitterApiClient twitterApiClient = TwitterCore.getInstance().getApiClient();
         StatusesService statusesService = twitterApiClient.getStatusesService();
 
-        statusesService.mentionsTimeline(200, // Count
-                 lastMentionID, // Since ID
+        statusesService.mentionsTimeline(5, // Count
+                lastMentionID, // Since ID
                 null, // Last ID
                 false, //Trim User
                 true, // Contributor Details
                 true, // Entities
                 new Callback<List<Tweet>>() {
-            @Override
-            public void success(Result<List<Tweet>> listResult) {
-                final List<Tweet> listTweets = listResult.data;
-                int i = 0;
-                for (Tweet tweet : listTweets) {
+                    @Override
+                    public void success(Result<List<Tweet>> listResult) {
+                        final List<Tweet> listTweets = listResult.data;
+                        if(!listTweets.isEmpty()) {
+                            splitTweetList.clear();
+                            splitTweet = null;
+                            for (Tweet tweet : listTweets) {
 
-                    String tweetText = tweet.text;
-                    splitTweet = tweetText.split("@\\w+");
-                    splitTweetList.add(i, splitTweet[1]);
-                    i++;
-                    myLayout.addView(new CompactTweetView(
-                            MainActivity.this,
+                                String tweetText = tweet.text;
+                                splitTweet = tweetText.split("@\\w+");
+                                splitTweetList.add(splitTweet[1]);
+
+                                tweetLayout.addView(new CompactTweetView(
+                                        MainActivity.this,
                                         tweet));
-                    if(tweet.id > lastMentionID) {
-                        lastMentionID = tweet.id;
+                                if (tweet.id > lastMentionID) {
+                                    lastMentionID = tweet.id;
+                                }
+                            }
+                        }
                     }
-                }
-            }
-            @Override
-            public void failure(TwitterException e) {
-                Toast.makeText(MainActivity.this, "Failed to get Twitter Mentions", Toast.LENGTH_SHORT).show();
 
-            }
-        });
+                    @Override
+                    public void failure(TwitterException e) {
+                        Toast.makeText(MainActivity.this, "Failed to get Twitter Mentions", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
     }
 
     @Override
@@ -123,7 +144,7 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -133,21 +154,46 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case (R.id.action_service):
+                Toast.makeText(this, "I don't do anything yet!", Toast.LENGTH_SHORT).show();
+                return true;
+            case (R.id.action_settings):
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
     public void playReceivedMusic(View view) {
-        String artist = splitTweetList.get(0);
-        musicHandler.playArtist(artist);
-
+        musicHandler.playArtist(splitTweetList.get(0));
     }
 
-    public void launchService(View view) {
-        Toast.makeText(MainActivity.this, "Hey Look I'm a button!", Toast.LENGTH_SHORT).show();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+                Calendar calendar = Calendar.getInstance();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd:MMMM:yyyy HH:mm:ss a");
+                final String strDate = simpleDateFormat.format(calendar.getTime());
+                getMentions();
+                playReceivedMusic(null);
+                Toast.makeText(getApplicationContext(), strDate, Toast.LENGTH_SHORT).show();
+                //if tweet received...
+                //  use regex to strip! ;)
+                //  generate an intent for Google Music
+                //else, don't do anything.
+                handler.postDelayed(this, timeDelay);
+        }
+    };
+
+    public void startHandler(View view) {
+        handler.postDelayed(runnable, 10000);
+        Toast.makeText(this, "Handler Started", Toast.LENGTH_SHORT).show();
+    }
+
+    public void stopHandler(View view) {
+        handler.removeCallbacks(runnable);
+        Toast.makeText(this, "Handler Stopped", Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/tinkerduck/twittermusicplayer/TweetPullService.java
+++ app/src/main/java/com/tinkerduck/twittermusicplayer/TweetPullService.java
@@ -4,8 +4,22 @@ import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterApiClient;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.core.services.StatusesService;
+import com.twitter.sdk.android.tweetui.CompactTweetView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Carl on 2/17/2015.
@@ -13,10 +27,14 @@ import com.twitter.sdk.android.core.TwitterSession;
 public class TweetPullService extends Service {
 
     private final TwitterSession session;
+    private boolean receivedTweet;
+    private MusicHandler musicHandler;
+    public String[] splitTweet;
+    public List<String> splitTweetList = new ArrayList<>();
 
     public TweetPullService(TwitterSession session){
         this.session = session;
-
+        musicHandler = new MusicHandler(this);
     }
 
     @Override
@@ -26,13 +44,61 @@ public class TweetPullService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        getMentions();
+        if(receivedTweet) {
+            playReceivedMusic();
+        } else {
+            stopSelf();
+        }
+        stopSelf();
         return super.onStartCommand(intent, flags, startId);
     }
-
-
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    public void playReceivedMusic() {
+        String artist = splitTweetList.get(0);
+        musicHandler.playArtist(artist);
+
+    }
+
+    private void getMentions() {
+        TwitterApiClient twitterApiClient = TwitterCore.getInstance().getApiClient();
+        StatusesService statusesService = twitterApiClient.getStatusesService();
+
+        statusesService.mentionsTimeline(200, // Count
+                1L, // Since ID
+                null, // Last ID
+                false, //Trim User
+                true, // Contributor Details
+                true, // Entities
+                new Callback<List<Tweet>>() {
+                    @Override
+                    public void success(Result<List<Tweet>> listResult) {
+                        final List<Tweet> listTweets = listResult.data;
+                        if (!listTweets.isEmpty()) { //If we received a tweet...
+                            Toast.makeText(TweetPullService.this, "Snagged a tweet!", Toast.LENGTH_SHORT).show();
+                            receivedTweet = true;
+                            for (Tweet tweet : listTweets) {
+
+                                String tweetText = tweet.text;
+                                splitTweet = tweetText.split("@\\w+");
+                                splitTweetList.add(splitTweet[1]);
+                            }
+                        } else { //If no tweets received...
+                            receivedTweet = false;
+                            Toast.makeText(TweetPullService.this, "No tweets received.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void failure(TwitterException e) {
+                        Toast.makeText(TweetPullService.this, "Failed to get Twitter Mentions", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
     }
 }
